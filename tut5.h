822b7bc570846cec487d135a79153f2799902797
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-05-23 22:21:08 +0200

    Add t family to tutorial 5

--- tut5.h
+++ tut5.h
@@ -8,6 +8,7 @@
     http://wordpress.discretization.de/ddg2019 tutorial 5.
 */
 
+
 /*! \fn float rho(int p)
     \brief The map \f$\rho\f$ from tutorial 5
 
@@ -159,7 +160,23 @@ vector2 phitilde5(vector2 z; int p) {
     \return the first component of the map \f$\tilde \varphi\f$
 */
 vector2 phitilde1(vector2 z; int p) {
-    return cdiv(phi1(z, p), phitilde5(z, p) - phitilde4(z, p));
+    return phi1(z, p);
+}
+
+
+/*! \fn vector2 gamma1(vector2 z; int p)
+    \brief Compute the first component of the map \f$\gamma\f$ from tutorial 5
+
+    Compute the first component of the map \f$\gamma\f$ from tutorial 5
+    on http://wordpress.discretization.de/ddg2019.
+
+    \param z the complex stereographic projection
+    \param p the natural parameter in the formula
+
+    \return the first component of the map \f$\gamma\f$
+*/
+vector2 gamma1(vector2 z; int p) {
+    return cdiv(phitilde1(z, p), phitilde5(z, p) - phitilde4(z, p));
 }
 
 
@@ -175,7 +192,23 @@ vector2 phitilde1(vector2 z; int p) {
     \return the second component of the map \f$\tilde \varphi\f$
 */
 vector2 phitilde2(vector2 z; int p) {
-    return cdiv(phi2(z, p), phitilde5(z, p) - phitilde4(z, p));
+    return phi2(z, p);
+}
+
+
+/*! \fn vector2 gamma2(vector2 z; int p)
+    \brief Compute the second component of the map \f$\gamma\f$ from tutorial 5
+
+    Compute the second component of the map \f$\gamma\f$ from tutorial 5
+    on http://wordpress.discretization.de/ddg2019.
+
+    \param z the complex stereographic projection
+    \param p the natural parameter in the formula
+
+    \return the second component of the map \f$\gamma\f$
+*/
+vector2 gamma2(vector2 z; int p) {
+    return cdiv(phitilde2(z, p), phitilde5(z, p) - phitilde4(z, p));
 }
 
 
@@ -191,33 +224,49 @@ vector2 phitilde2(vector2 z; int p) {
     \return the third component of the map \f$\tilde \varphi\f$
 */
 vector2 phitilde3(vector2 z; int p) {
-    return cdiv(phi3(z, p), phitilde5(z, p) - phitilde4(z, p));
+    return phi3(z, p);
 }
 
 
-/*! \fn vector phitilde(vector2 z; int p)
-    \brief Compute the map \f$\tilde \varphi\f$ from tutorial 5
+/*! \fn vector2 gamma3(vector2 z; int p)
+    \brief Compute the third component of the map \f$\gamma\f$ from tutorial 5
 
-    Compute the map \f$\tilde \varphi\f$ from tutorial 5 on
+    Compute the third component of the map \f$\gamma\f$ from tutorial 5
+    on http://wordpress.discretization.de/ddg2019.
+
+    \param z the complex stereographic projection
+    \param p the natural parameter in the formula
+
+    \return the third component of the map \f$\gamma\f$
+*/
+vector2 gamma3(vector2 z; int p) {
+    return cdiv(phitilde3(z, p), phitilde5(z, p) - phitilde4(z, p));
+}
+
+
+/*! \fn vector gamma(vector2 z; int p)
+    \brief Compute the map \f$\gamma\f$ from tutorial 5
+
+    Compute the map \f$\gamma\f$ from tutorial 5 on
     http://wordpress.discretization.de/ddg2019.
 
     \param z the complex stereographic projection
     \param p the natural parameter in the formula
 
-    \return the result vector of the map \f$\tilde \varphi\f$
+    \return the result vector of the map \f$\gamma\f$
 */
-vector phitilde(vector2 z; int p) {
-    return set(real(phitilde1(z, p)), real(phitilde2(z, p)),
-        real(phitilde3(z, p)));
+vector gamma(vector2 z; int p) {
+    return set(real(gamma1(z, p)), real(gamma2(z, p)),
+        real(gamma3(z, p)));
 }
 
 
-/*! \fn vector phitilde_family(vector2 c; vector2 z; int p)
-    \brief Compute the associated family of the surface
+/*! \fn vector phitilde_s_family(vector2 c; vector2 z; int p)
+    \brief Compute the associated family of the surface for \f$s\f$
 
     Compute the associated family of the surface constructed via the map
-    \f$\tilde \varphi\f$ from tutorial5 on
-    http://wordpress.discretization.de/ddg2019.
+    \f$c \tilde \varphi\f$ from tutorial5 on
+    http://wordpress.discretization.de/ddg2019 with \f$c = s e^i\f$.
 
     \param c the point on the 1-sphere to multiply with \f$\tilde \varphi\f$
     \param z the complex stereographic projection
@@ -225,9 +274,40 @@ vector phitilde(vector2 z; int p) {
 
     \return the result vector
 */
-vector phitilde_family(vector2 c; vector2 z; int p) {
-    float real_c_phitilde1 = real(cmul(c, phitilde1(z, p)));
-    float real_c_phitilde2 = real(cmul(c, phitilde2(z, p)));
-    float real_c_phitilde3 = real(cmul(c, phitilde3(z, p)));
-    return set(real_c_phitilde1, real_c_phitilde2, real_c_phitilde3);
+vector gamma_s_family(vector2 c; vector2 z; int p) {
+    float real_c_gamma1 = real(cmul(c, gamma1(z, p)));
+    float real_c_gamma2 = real(cmul(c, gamma2(z, p)));
+    float real_c_gamma3 = real(cmul(c, gamma3(z, p)));
+    return set(real_c_gamma1, real_c_gamma2, real_c_gamma3);
+}
+
+
+/*! \fn vector gamma_t_family(float t; c vector2 z; int p)
+    \brief Compute the associated family of the surface for \f$t\f$
+
+    Compute the associated family of the surface constructed via the map
+    \f$\gamma\f$ with parameter \f$t\f$ from tutorial5 on
+    http://wordpress.discretization.de/ddg2019.
+
+    \param t the real parameter
+    \param z the complex stereographic projection
+    \param p the natural parameter in the formula
+
+    \return the result vector
+*/
+vector gamma_t_family(float t; vector2 z; int p) {
+    float cos_t = cos(t);
+    float sin_t = sin(t);
+    vector2 t_phitilde1 = phitilde1(z, p);
+    vector2 t_phitilde2 = cmul(set(cos_t, 0), phitilde2(z, p))
+        - cmul(phitilde4(z, p), set(sin_t, 0));
+    vector2 t_phitilde3 = phitilde3(z, p);
+    vector2 t_phitilde4 = cmul(phitilde2(z, p), set(sin_t, 0))
+        - cmul(phitilde4(z, p), set(cos_t, 0));
+    vector2 t_phitilde5 = phitilde5(z, p);
+    vector2 divisor = t_phitilde5 - t_phitilde4;
+    float real_t_gamma1 = real(cdiv(t_phitilde1, divisor));
+    float real_t_gamma2 = real(cdiv(t_phitilde2, divisor));
+    float real_t_gamma3 = real(cdiv(t_phitilde3, divisor));
+    return set(real_t_gamma1, real_t_gamma2, real_t_gamma3);
 }
