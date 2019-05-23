f305f3319765f7276ef0ad2b1104ddc012d68a24
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-05-23 22:25:25 +0200

    Update docs

--- docs/tut6_8h.js
+++ docs/tut6_8h.js
@@ -1,17 +1,4 @@
 var tut6_8h =
 [
-    [ "phi1", "tut6_8h.html#a42d12adc813581457c3804188cb98257", null ],
-    [ "phi2", "tut6_8h.html#a0c9b6f4e27eda6dd105c046019dbf425", null ],
-    [ "phi3", "tut6_8h.html#ac20306223ecd47281b297db6a2ad50a6", null ],
-    [ "phi4", "tut6_8h.html#a2ceb52d42558c2487b7b31e4157af2dd", null ],
-    [ "phi5", "tut6_8h.html#a94f72cbe6b34f19f455c08a0e217c8be", null ],
-    [ "phitilde", "tut6_8h.html#aea77a2a6ff09ba7e1e6dcf660e6a275c", null ],
-    [ "phitilde1", "tut6_8h.html#a1432954e9b420f28daf2efd6144a0531", null ],
-    [ "phitilde2", "tut6_8h.html#afc81f7751d8926171bb2cf7b3087e424", null ],
-    [ "phitilde3", "tut6_8h.html#aaa76059c84163e72044257ab25d2d92a", null ],
-    [ "phitilde4", "tut6_8h.html#a0d1e09058661e8d6a2dd357808817325", null ],
-    [ "phitilde5", "tut6_8h.html#a93c6a017a84c23f861f49893b1c1e635", null ],
-    [ "phitilde_family", "tut6_8h.html#a3db90bd17c97526b2079670714443109", null ],
-    [ "rho", "tut6_8h.html#a0f0ee9fde11157a84a3450e68f11e577", null ],
-    [ "rr", "tut6_8h.html#acd48101ff3bb6ce92a071eab50deac46", null ]
+    [ "rho", "tut6_8h.html#a0f0ee9fde11157a84a3450e68f11e577", null ]
 ];
